5cb6f66cc1bf1566274b81ec42b694f2b28d3a77
Unioviedu <UO244594@gmail.com>
2019-01-14 22:10:56 +0100

    Cambio url de acceso

--- src/main/java/com/uniovi/quizapp/config/security/CORSResponseFilter.java
+++ src/main/java/com/uniovi/quizapp/config/security/CORSResponseFilter.java
@@ -25,7 +25,7 @@ public class CORSResponseFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
-		res.addHeader("Access-Control-Allow-Origin", "https://quiz-app-uniovi.herokuapp.com");
+		res.addHeader("Access-Control-Allow-Origin", "https://quiz-app-web-uniovi.herokuapp.com");
 		res.addHeader("Access-Control-Allow-Credentials", "true");
 		res.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
 		res.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
