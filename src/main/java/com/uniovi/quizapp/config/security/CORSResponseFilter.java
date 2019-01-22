b00ffb00ba0107fb6700f6644a3384eee3bda5fe
Edu <UO244594@uniovi.es>
2019-01-22 17:59:07 +0100

    Cambio headers

--- src/main/java/com/uniovi/quizapp/config/security/CORSResponseFilter.java
+++ src/main/java/com/uniovi/quizapp/config/security/CORSResponseFilter.java
@@ -1,7 +1,6 @@
 package com.uniovi.quizapp.config.security;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -25,8 +24,8 @@ public class CORSResponseFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
-		//res.addHeader("Access-Control-Allow-Origin", "https://quiz-app-web-uniovi.herokuapp.com");
-		res.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		res.addHeader("Access-Control-Allow-Origin", "https://quiz-app-web-uniovi.herokuapp.com");
+		//res.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 		res.addHeader("Access-Control-Allow-Credentials", "true");
 		res.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
 		res.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
